3b9ded58b35c7cd85d3016e1203230e42dcfbbbf
CoalGravel <cgrav3@ocdsb.ca>
2020-10-19 02:16:36 -0400

    Added basic commands to the bot

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import discord
-import bot_settings
+import random
+import bot_settings #local file .gitignore'd as it contains the bot token
 from discord.ext import commands
 
-client = commands.Bot(command_prefix='.')
+client = commands.Bot(command_prefix='!')
 
 @client.event
 async def on_ready():
@@ -15,5 +16,33 @@ async def on_member_join(member):
 @client.event
 async def on_member_remove(member):
     print(f"{member} has left a server.")
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"Pong! {round(client.latency*1000)}ms")
+
+@client.command(aliases=['8ball','eightball'])
+async def eight_ball(ctx, *, question):
+    responses = ["It is certain.",
+                "It is decidedly so.",
+                "Without a doubt.",
+                "Yes - definitely.",
+                "You may rely on it.",
+                "As I see it, yes.",
+                "Most likely.",
+                "Outlook good.",
+                "Yes.",
+                "Signs point to yes.",
+                "Reply hazy, try again.",
+                "Ask again later.",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again.",
+                "Don't count on it.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Very doubtful."]
+    await ctx.send(random.choice(responses))
 
 client.run(bot_settings.token)
---
+++ bot_settings_example.py
@@ -0,0 +1,3 @@
+#Example of contents of bot_settings.py, as it is excluded from the repository due to sensitive data
+
+token = "PASTE YOUR DISCORD BOT TOKEN HERE"
